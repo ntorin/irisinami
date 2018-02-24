b681ac60f2c5698e20023180b50050a9d32812fc
Iris Inami <irisinami@Iriss-Mac.local>
2018-02-24 06:07:24 -0800

    adding redux, routing, thunk; configuring react webpacker

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'landing#index'
+
+  constraints subdomain: 'blog' do
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

6abf9df8de2b92dd2e5ff16bc1983287eb3fb1f2
Iris Inami <irisinami@Iriss-Mac.local>
2018-02-28 04:29:37 -0800

    removing node_modules, tmp, log folders from repo

--- node_modules/react-mad-navbar/example/actions/index.js
+++
@@ -1,8 +0,0 @@
-export function windowResize(size) {
-  return {type: 'windowResize', size}
-}
-
-export function windowInitialSize(initialSize) {
-  return {type: 'windowInitialSize', initialSize}
-}
-
